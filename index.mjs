7146965532b3b7c99fff3fa97feb07e924840391
tristan <69066026+twisttaan@users.noreply.github.com>
2022-07-26 16:04:50 +1000

    feat: backfill

--- index.mjs
+++ index.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { Client } from "discord.js";
+import { Client, GatewayDispatchEvents, GatewayOpcodes } from "discord.js";
 import WebSocket from "ws";
 
 let readyPayload = {};
+let helloPayload = {};
+let backfill = [];
 
 const client = new Client({
   intents: parseInt(process.env.DISCORD_INTENTS || "37383"),
@@ -13,37 +15,116 @@ const wss = new WebSocket.Server({
   port: parseInt(process.env.PORT || "6969"),
 });
 
+function broadcastClients(packet) {
+  console.log(packet);
+
+  wss.clients.forEach((ws) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(packet));
+    }
+  });
+}
+
+const DONT_SEND_TO_CLIENT_PARK_OPCODES = [GatewayOpcodes.HeartbeatAck];
+const BACKFILL_EVENTS = [GatewayDispatchEvents.GuildCreate];
+
+/**
+ * Discord -> Park
+ */
 client.on("raw", (packet) => {
-  if (packet.t === "READY") {
-    console.log("Ready payload received from Discord.");
-    readyPayload = packet;
+  if (packet.op === GatewayOpcodes.Hello) {
+    console.log("[Discord -> Park] Hello payload received from Discord.");
+    helloPayload = packet;
+    return;
   }
+
+  if (packet.t === GatewayDispatchEvents.Ready) {
+    console.log(
+      `[Discord -> Park] Ready payload received from Discord. | ${packet.d.guilds.length} guilds.`
+    );
+    readyPayload = packet;
+    return;
+  }
+
+  if (DONT_SEND_TO_CLIENT_PARK_OPCODES.includes(packet.op)) {
+    return;
+  }
+
+  if (BACKFILL_EVENTS.includes(packet.t)) {
+    backfill.push(packet);
+    return;
+  }
+
+  broadcastClients(packet);
 });
 
-wss.on("connection", (ws) => {
-  console.log(`New connection! Sending ready payload`);
+/**
+ * Park -> Discord
+ */
 
+const DONT_SEND_TO_DISCORD_OPCODES = [
+  GatewayOpcodes.Heartbeat,
+  GatewayOpcodes.HeartbeatAck,
+];
+
+let currentHeartbeat = 0;
+
+/**
+ * @param {WebSocket} ws
+ */
+function handleHeartbeat(ws) {
   ws.send(
     JSON.stringify({
-      op: 10,
-      d: {
-        heartbeat_interval: 45000,
-      },
+      op: GatewayOpcodes.HeartbeatAck,
+      d: currentHeartbeat === 0 ? null : currentHeartbeat,
     })
   );
+  console.log(
+    `[Park -> Client] Ack'd heartbeat | Up to ${currentHeartbeat} beats.`
+  );
+  currentHeartbeat++;
+}
 
-  client.on("raw", (packet) => {
-    console.log(`Forwarding packet ${JSON.stringify(packet)}`);
-    ws.send(JSON.stringify(packet));
-  });
+wss.on("connection", (ws) => {
+  console.log(`[Client 🤝 Park] New connection! Sending hello payload`);
+  ws.send(JSON.stringify(helloPayload));
 
   ws.on("message", (message) => {
     const buffer = JSON.parse(message.toString());
 
-    if (buffer.op === 2) {
-      console.log(`Sending ready payload!`);
+    if (buffer.op === GatewayOpcodes.Identify) {
+      console.log(
+        `[Cache -> Client] Sending ready payload! Then backfilling guilds.`
+      );
       ws.send(JSON.stringify(readyPayload));
+      backfill.forEach((packet) => {
+        ws.send(JSON.stringify(packet));
+      });
+      return;
     }
+
+    if (buffer.op === GatewayOpcodes.Heartbeat) {
+      return void handleHeartbeat(ws);
+    }
+
+    if (DONT_SEND_TO_DISCORD_OPCODES.includes(buffer.op)) {
+      return;
+    }
+
+    if (buffer.d && buffer.d.token) {
+      buffer.d.token = client.token;
+    }
+
+    console.log(buffer);
+
+    console.log(
+      `[Client -> Discord] Forwarding packet ${
+        buffer.t || buffer.op || "UNKNOWN"
+      }`
+    );
+
+    // @ts-ignore - Accessing private property
+    client.ws.broadcast(JSON.stringify(buffer));
   });
 });
 
